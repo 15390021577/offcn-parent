7a7381bdc6f8460a430e05ef392233caab872627
offcn-java <james@qq.com>
2021-03-05 16:31:04 +0800

    add by james

--- scw-common/src/main/java/com/offcn/utils/MyTest2.java
+++ scw-common/src/main/java/com/offcn/utils/MyTest2.java
@@ -2,6 +2,6 @@ package com.offcn.utils;
 
 public class MyTest2 {
     public static void main(String[] args) {
-        System.out.println("==================");
+        System.out.println("==================111111111111");
     }
 }
